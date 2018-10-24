f14901bbcc08bab219594ea1a4b3696c5ec98c39
Tariq Abulaban <TariqAbulaban@server.ca>
2018-10-23 17:51:52 -0700

    front end done

--- voyageurs/App.js
+++ voyageurs/App.js
@@ -1,63 +1,122 @@
 import React from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import Geocoder from 'react-native-geocoding';
-var timetable2 ='{"days":[{ "Courses":[{"courseTitle": "Econ 101", "building" : "brock hall", "address":"1874 E Mall, Vancouver, BC V6T 1Z1","startTime"  : "10:00","endTime"    : "12:00"}, {"courseTitle": "cpen 331", "building" : "Macleod", "address":"2356 Main Mall, Vancouver, BC V6T 1Z4","startTime"  : "12:00","endTime"    : "14:00"}] }]}';
-var obj = JSON.parse(timetable2);
+import MapViewDirections from 'react-native-maps-directions';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View, Button, Alert } from 'react-native';
+  var timetable2 ='{"days":[{ "Courses":[{"courseTitle": "Econ 101", "building" : "brock hall", "address":"1874 E Mall, Vancouver, BC V6T 1Z1","startTime"  : "10:00","endTime"    : "12:00"}, {"courseTitle": "cpen 331", "building" : "Macleod", "address":"2356 Main Mall, Vancouver, BC V6T 1Z4","startTime"  : "12:00","endTime"    : "14:00"}] }]}';
+  var obj = JSON.parse(timetable2);
 
-// const timetable = require( './courses.json');
-Geocoder.init('AIzaSyCvW9JtKWa3ftr-FD-bGsFqR9EBQMlGn7k'); // use a valid API key
 
-// const timetableparsed = JSON.parse(this.timetable);
+const LATITUDEDELTA =  0.0122
+const LONGITUDEDELTA = .0221
+const GOOGLE_MAPS_APIKEY = 'AIzaSyCvW9JtKWa3ftr-FD-bGsFqR9EBQMlGn7k'
+Geocoder.init(GOOGLE_MAPS_APIKEY); // use a valid API key
+
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
 
-  // var date = new Date(new Date().toLocaleString());
-  //
-  // var day = date.getDay();
-  // var minutes = date.getMinutes()
-  // var hours = date.getHours() // 24 hour clock
-  // const word = data.name;
-  JsonReader = () => {
-    Alert.alert(obj.days[0].Courses[1].building);
+    this.state = {
+      initialPosition: {
+        latitude: 0,
+        longitude: 0,
+        latitudeDelta: 0,
+        longitudeDelta: 0,
+      },
+      markerPosition: {
+        latitude: 0,
+        longitude: 0,
+      },
+      destination: {
+        latitude: 49.2580,
+        longitude: -123.253,
+      }
+    }
   }
+componentDidMount(){
+  navigator.geolocation.getCurrentPosition((position) => {
+    var lat = parseFloat(position.coords.latitude )
+    var long = parseFloat(position.coords.longitude)
 
-  Geocoding
+    var initialRegion = {
+      latitude: lat,
+      longitude: long,
+      latitudeDelta: LATITUDEDELTA,
+      longitudeDelta: LONGITUDEDELTA,
+    }
 
+    this.setState({initialPosition : initialRegion})
+    this.setState({markerPosition: initialRegion})
+  }, (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true,  maximumAge: 1000})
+}
+
+getDestination = () => {
+  Geocoder.from(obj.days[0].Courses[1].building + " ubc")
+      .then(json => {
+        var location = json.results[0].geometry.location;
+        //Alert.alert(JSON.stringify(location));
+
+        console.log("geocoder values: ");
+        console.log(location.lat);
+        console.log(location.lng);
+
+        var destinationResult = {
+          latitude: location.lat,
+          longitude: location.lng,
+        }
+
+        this.setState({destination: destinationResult})
+
+        console.log("state values: ");
+        console.log("destination: " + this.state.destination.latitude);
+        console.log("destination: " + this.state.destination.longitude);
+      })
+      .catch(error => console.warn(error));
+}
 
   render() {
     return (
       <View style={styles.container}>
-      <Button title="hello"
-      onPress = {this.JsonReader}
-      />
-{/*
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: 49.2606,
-          longitude: -123.2460,
-          latitudeDelta: 0.0122,
-          longitudeDelta: .0221,
-        }}>
+        region= {this.state.initialPosition}
+        showUserLocation = {true}
+        followsUserLocation = {true}  >
         <MapView.Marker
-          coordinate = {{
-            latitude: 49.2606,
-            longitude: -123.2460,
-          }}>
-
-          {  /*<View style={styles.radius}>
+          coordinate = {this.state.markerPosition}>
+            <View style={styles.radius}>
             <View style={styles.marker}/>
-            </View>   this code is for the starting location
-            it shows a blue circle, without this code,
-            it just shows a red marker
-           </MapView.Marker>
-        </MapView>
-*/}
+            </View>
+        </MapView.Marker>
+        <MapView.Marker
+         coordinate = {this.state.destination}>
 
+          </MapView.Marker>
+          <MapViewDirections
+            origin={this.state.markerPosition}
+            destination={this.state.destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            mode={"bicycling"}
+            strokeWidth={6}
+            strokeColor={"red"}
+            />
+        </MapView>
+
+        <View style={ styles.bottomView}>
+                  <Button
+                    title="Get Next Class"
+                    onPress = {this.getDestination} />
+                </View>
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   radius: {
@@ -92,7 +151,15 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     top: 0,
-    bottom: 0,
+    bottom: 80,
     position: 'absolute'
+  },
+  bottomView:{
+    width: '100%',
+    height: 80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0
   }
 });
