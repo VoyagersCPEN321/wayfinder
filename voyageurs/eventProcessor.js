e9e51e8442ebff46e5ec949d5ffccdfbe2f51278
Tariq Abulaban <TariqAbulaban@server.ca>
2018-10-28 19:16:25 -0700

    removed semi colon

--- voyageurs/eventProcessor.js
+++ voyageurs/eventProcessor.js
@@ -20,12 +20,12 @@ function isBeforeDay(a, b) {
     }
 
     return false;
-};
+}
 
 function dayToUTCDay(day) {
 
   dict = {"MO": 1, "TU": 2, "WE": 3, "TH": 4, "FR":5, "SA": 6, "SU": 7 };
-  
+
   if(dict[day]){
     return dict[day];
   }
