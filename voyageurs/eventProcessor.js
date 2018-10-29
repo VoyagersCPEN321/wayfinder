4751a2174926fdd2ccfae489e6cb414bd01a8b8a
Ali Mohamed <alihossam_10@live.com>
2018-10-28 19:54:30 -0700

    Fix cyclomatic complexity issue

--- voyageurs/eventProcessor.js
+++ voyageurs/eventProcessor.js
@@ -3,30 +3,41 @@
  * Checks if a is before b in a day level comparison.
  */
 function isBeforeDay(a, b) {
-    if(b.getFullYear() > a.getFullYear) {
-      return true;
-    }
-    if(b.getFullYear() < a.getFullYear) {
-      return false;
-    }
-    if(b.getMonth() > a.getMonth()) {
-      return true;
-    }
-    if(b.getMonth() < a.getMonth()) {
-      return false;
-    }
-    if(b.getDay() > a.getDay()) {
-      return true;
-    }
+  if (b.getFullYear() !== a.getFullYear) {
+    return isBeforeYear(a, b);
+  }
+  if (b.getMonth() !== a.getMonth()) {
+    return isBeforeMonth(a, b);
+  }
+  if (b.getDay() > a.getDay()) {
+    return true;
+  }
+  return false;
+}
 
+function isBeforeYear(a, b) {
+  if (b.getFullYear() > a.getFullYear) {
+    return true;
+  }
+  if (b.getFullYear() < a.getFullYear) {
     return false;
+  }
+}
+
+function isBeforeMonth(a, b) {
+  if (b.getMonth() > a.getMonth()) {
+    return true;
+  }
+  if (b.getMonth() < a.getMonth()) {
+    return false;
+  }
 }
 
 function dayToUTCDay(day) {
 
-  dict = {"MO": 1, "TU": 2, "WE": 3, "TH": 4, "FR":5, "SA": 6, "SU": 7 };
+  dict = { "MO": 1, "TU": 2, "WE": 3, "TH": 4, "FR": 5, "SA": 6, "SU": 7 };
 
-  if(dict[day]){
+  if (dict[day]) {
     return dict[day];
   }
 
@@ -34,26 +45,26 @@ function dayToUTCDay(day) {
 }
 
 export function isHappeningToday(event) {
-    let today = new Date();
-    if(!event) {
-      throw new Error("null event");
-    }
-    /* Check if the event's first occurence hasn't occurred yet. */
-    var startDay = new Date(event.startDay);
-    var lastDay = new Date(event.lastDay);
+  let today = new Date();
+  if (!event) {
+    throw new Error("null event");
+  }
+  /* Check if the event's first occurence hasn't occurred yet. */
+  var startDay = new Date(event.startDay);
+  var lastDay = new Date(event.lastDay);
 
-    if (isBeforeDay(today, startDay)){
-      return false;
-    }
-    /* Check if the event's last occurrence already passed */
-    if (isBeforeDay(lastDay, today)) {
-      return false;
-    }
+  if (isBeforeDay(today, startDay)) {
+    return false;
+  }
+  /* Check if the event's last occurrence already passed */
+  if (isBeforeDay(lastDay, today)) {
+    return false;
+  }
 
-    /* We know the event is within its ocurrence range */
-    if (today.getDay() !== dayToUTCDay(event.day)) {
-      return false;
-    }
-    // TODO check for non weekly recurrence, for now assume all events are weekly
-    return true;
+  /* We know the event is within its ocurrence range */
+  if (today.getDay() !== dayToUTCDay(event.day)) {
+    return false;
+  }
+  // TODO check for non weekly recurrence, for now assume all events are weekly
+  return true;
 }
