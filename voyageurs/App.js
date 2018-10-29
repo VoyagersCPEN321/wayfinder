1c1a043f93cf48e7745233e5c0c3d770cbf41467
Ali Mohamed <alihossam_10@live.com>
2018-10-28 19:48:30 -0700

    Fix minor bug (duplicate key)

--- voyageurs/App.js
+++ voyageurs/App.js
@@ -262,7 +262,6 @@ const styles = StyleSheet.create({
   calloutMessage: {
     borderColor: "transparent",
     marginLeft: 10,
-    marginRight: 10,
     marginTop: 10,
     marginBottom: 10,
     marginRight: 10,
