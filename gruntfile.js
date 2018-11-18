b83241753808a08195b1ccce11f1d5bca05a50dd
Ali Mohamed <alihossam_10@live.com>
2018-11-17 16:00:29 -0800

    Update gruntfile to compile test files

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
       ts: {
         app: {
           files: [{
-            src: ["src/\*\*/\*.ts", "!src/.baseDir.ts", "!src/_all.d.ts"],
+            src: ["src/\*\*/\*.ts", "!src/.baseDir.ts", "!src/_all.d.ts", "tests/\*\*/\*.ts"],
             dest: "./output"
           }],
           options: {
