4e89ddbfc98c60b05ee77287bdb6fc8f3c5e2fd2
kevintabouguia <kevinyok@alumni.ubc.ca>
2018-11-24 19:32:10 -0800

    Started implementing pus notifications

--- voyageurs/LoginScreen.js
+++ voyageurs/LoginScreen.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 import { AsyncStorage } from "react-native";
 import * as CONSTANTS from "./constants";
+import { Permissions, Notifications } from 'expo';
 
 const APP_ID = "171287213807359";
 const FB_AUTH = "/auth/fb/";
@@ -41,7 +42,7 @@ export default class LoginScreen extends Component {
   loginFailedAlert = () => {
     Alert.alert("Login Failed please try again");
   }
-
+    
   logIn = async function (view) {
     try {
       const {
@@ -51,9 +52,14 @@ export default class LoginScreen extends Component {
         permissions: ['public_profile']
       });
       if (type === 'success') {
+
+        const {
+          pushToken
+        } = await registerForPushNotificationsAsync();
+
         this.setState({ loading : true});
         /* Request JWT from server */
-        await view.getJWT(token).then(() => {
+        await view.getJWT(token, pushToken).then(() => {
           if(!view.state.token) {
             view.loginFailedAlert();
             this.setState({ loading : false});
@@ -71,12 +77,17 @@ export default class LoginScreen extends Component {
     }
   }
 
-  getJWT = async (fbToken) => {
+  getJWT = async (fbToken, pushToken) => {
     return fetch(CONSTANTS.APP_URL + FB_AUTH, {
       method: "POST",
       headers: new Headers({
-        'Authorization': 'Bearer ' + fbToken
-      })
+        'Authorization': 'Bearer ' + fbToken,
+        Accept: 'application/json',
+      'Content-Type': 'application/json',
+      }), 
+      body: JSON.stringify({
+        token: pushToken 
+      }),
     }).then((res) => {
       if (res.status == 200) {
         return this.extractToken(res);
@@ -100,6 +111,8 @@ export default class LoginScreen extends Component {
       }
     });
   }
+
+  
 
   renderBusyIndicator = () => {
     if(this.state.loading) {
@@ -154,3 +167,23 @@ const styles = StyleSheet.create({
   }
 });
 
+async function registerForPushNotificationsAsync() {
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== 'granted') {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
+
+  if (finalStatus !== 'granted') {
+    console.log("Push Notifications permissions not granted");
+    return;
+  }
+
+  let token = await Notifications.getExpoPushTokenAsync();
+
+  return token;
+}
