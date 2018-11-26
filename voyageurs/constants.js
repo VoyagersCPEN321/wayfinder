58907f2a5c28770550e54169a644539e3684405e
kevintabouguia <kevinyok@alumni.ubc.ca>
2018-11-25 18:20:46 -0800

    update app url

--- voyageurs/constants.js
+++ voyageurs/constants.js
@@ -1,5 +1,5 @@
 /* List of global consts. */
-export const APP_URL = "http://localhost:8080";//"http://192.168.0.12:8080";
-//export const APP_URL = "http://wayfinder.eastus.cloudapp.azure.com:8080";//"http://192.168.0.12:8080";
+//export const APP_URL = "http://localhost:8080";//"http://192.168.0.12:8080";
+export const APP_URL = "http://wayfinder.eastus.cloudapp.azure.com:8080";//"http://192.168.0.12:8080";
 export const TOKEN_LOCATION = '@tokenStore:token';
 export const SCHEDULE_LOCATION = '@userSchedule:schedule';
